74816d4716402d0aed4ca0b7ff0dffd791a3d8c9
HansSulistio <hanslsulistio98@gmail.com>
2020-05-29 00:02:21 +0800

    add home

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react"
-import Canvas from "../Canvas/canvas"
+import FontAwesome from "react-fontawesome"
+import "../Scss/home.scss"
+import AliceCarousel from 'react-alice-carousel';
+
+// import Canvas from "../Canvas/canvas"
+// import Sidebar from "../Hoc/sidebar"
 class Home extends Component {
     state = {
-
+        visible: false
     };
+
+    renderGoal = () => {
+        var template = [];
+        for (var i = 0; i < 5; i++) {
+            template.push(
+                <div className="goal--wrapper">
+                    <div className="goal--meter">
+                        <p>30</p><p>Days remaining</p>
+                    </div>
+                    <div className="goal--title">
+                        <p>push up</p>
+                    </div>
+                </div>);
+        }
+
+        return template
+    }
+
 
     render() {
         return (
-            <div>Home
-                <Canvas />
+            <div>
+                <AliceCarousel
+                    startIndex={1}
+                    fadeOutAnimation={true}
+                    mouseDragEnabled={true}
+                    buttonsDisabled={true}
+                >
+                    {this.renderGoal()}
+                </AliceCarousel>
+                <FontAwesome name="clock-o" />
+                {/* <button onClick={() => this.toggleSidebar()} >click me</button> */}
             </div>
         )
     }
---
+++ src/Hoc/sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div>test</div>
+    )
+}
+
+export default Sidebar
